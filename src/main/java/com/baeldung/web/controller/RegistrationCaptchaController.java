d044278bd471cf1a05a8667d0b4231f3437b1ad5
iBranch211 <leo.esaki211@gmail.com>
2020-03-04 21:51:04 +0100

    JAVA-864: Fix failing integration tests

---
+++ src/main/java/com/baeldung/web/controller/RegistrationCaptchaController.java
@@ -0,0 +1,58 @@
+package com.baeldung.web.controller;
+
+import com.baeldung.captcha.ICaptchaService;
+import com.baeldung.persistence.model.User;
+import com.baeldung.registration.OnRegistrationCompleteEvent;
+import com.baeldung.service.IUserService;
+import com.baeldung.web.dto.UserDto;
+import com.baeldung.web.util.GenericResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class RegistrationCaptchaController {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private ICaptchaService captchaService;
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    public RegistrationCaptchaController() {
+        super();
+    }
+
+    // Registration
+
+    @RequestMapping(value = "/user/registrationCaptcha", method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse captchaRegisterUserAccount(@Valid final UserDto accountDto, final HttpServletRequest request) {
+
+        final String response = request.getParameter("g-recaptcha-response");
+        captchaService.processResponse(response);
+
+        LOGGER.debug("Registering user account with information: {}", accountDto);
+
+        final User registered = userService.registerNewUserAccount(accountDto);
+        eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
+        return new GenericResponse("success");
+    }
+
+    private String getAppUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+    }
+
+}
--- src/main/java/org/baeldung/security/ISecurityUserService.java
+++
@@ -1,7 +0,0 @@
-package org.baeldung.security;
-
-public interface ISecurityUserService {
-
-    String validatePasswordResetToken(long id, String token);
-
-}
