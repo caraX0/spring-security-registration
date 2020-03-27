23c492f93b81b26e6602987e0a7adacd7f4558c6
Addis-Belete <addis.bel@gmail.com>
2020-03-27 09:47:55 +0100

    JAVA-845: Extract user creation to the private method

--- src/main/java/com/baeldung/web/controller/OldRegistrationController.java
+++ src/main/java/com/baeldung/web/controller/OldRegistrationController.java
@@ -1,18 +1,12 @@
 package com.baeldung.web.controller;
 
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 import com.baeldung.persistence.model.PasswordResetToken;
 import com.baeldung.persistence.model.User;
 import com.baeldung.persistence.model.VerificationToken;
 import com.baeldung.registration.OnRegistrationCompleteEvent;
 import com.baeldung.service.IUserService;
 import com.baeldung.web.dto.UserDto;
+import com.baeldung.web.error.UserAlreadyExistException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,13 +24,18 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/old")
@@ -105,9 +104,8 @@ public class OldRegistrationController {
     public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid final UserDto userDto, final HttpServletRequest request, final Errors errors) {
         LOGGER.debug("Registering user account with information: {}", userDto);
 
-        final User registered = userService.registerNewUserAccount(userDto);
+        final User registered = createUserAccount(userDto);
         if (registered == null) {
-            // result.rejectValue("email", "message.regError");
             return new ModelAndView("registration", "user", userDto);
         }
         try {
@@ -224,5 +222,15 @@ public class OldRegistrationController {
         email.setText(message + " \r\n" + url);
         email.setFrom(env.getProperty("support.email"));
         return email;
+    }
+
+    private User createUserAccount(UserDto userDto) {
+        User registered = null;
+        try {
+            registered = userService.registerNewUserAccount(userDto);
+        } catch (UserAlreadyExistException e) {
+            return null;
+        }
+        return registered;
     }
 }
