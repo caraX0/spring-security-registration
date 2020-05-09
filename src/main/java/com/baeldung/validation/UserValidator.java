3366aea9d0d5de8a41b7f1fbb9eed3e038d795b8
iBranch211 <leo.esaki211@gmail.com>
2020-05-09 09:32:25 +0300

    fix typos

--- src/main/java/com/baeldung/validation/UserValidator.java
+++ src/main/java/com/baeldung/validation/UserValidator.java
@@ -14,9 +14,9 @@ public class UserValidator implements Validator {
 
     @Override
     public void validate(final Object obj, final Errors errors) {
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "message.firstName", "Firstname is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "message.lastName", "LastName is required.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "message.password", "LastName is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "message.firstName", "First Name is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "message.lastName", "Last Name is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "message.password", "Last Name is required.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "message.username", "UserName is required.");
     }
 
