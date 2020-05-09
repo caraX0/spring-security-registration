d384af88edd05539e3478e95d62115d01a13a4c0
iBranch211 <leo.esaki211@gmail.com>
2020-05-09 09:49:50 +0300

    NewLocationTokenRepository: fix UserLocation

--- src/main/java/com/baeldung/persistence/dao/NewLocationTokenRepository.java
+++ src/main/java/com/baeldung/persistence/dao/NewLocationTokenRepository.java
@@ -1,13 +1,13 @@
 package com.baeldung.persistence.dao;
 
 import com.baeldung.persistence.model.NewLocationToken;
-import com.baeldung.persistence.model.User;
+import com.baeldung.persistence.model.UserLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewLocationTokenRepository extends JpaRepository<NewLocationToken, Long> {
 
     NewLocationToken findByToken(String token);
 
-    NewLocationToken findByUserLocation(User userLocation);
+    NewLocationToken findByUserLocation(UserLocation userLocation);
 
 }
