77bc0ba3ea1cccab8d0476c3ca988bb8044aada3
iBranch211 <leo.esaki211@gmail.com>
2023-02-02 19:56:33 +0200

    JAVA-17513 Block an attacker after exceed the MAX_ATTEMPT (#205)

--- src/main/java/com/baeldung/security/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/baeldung/security/CustomAuthenticationFailureHandler.java
@@ -24,6 +24,12 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
     @Autowired
     private LocaleResolver localeResolver;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
     @Override
     public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException exception) throws IOException, ServletException {
         setDefaultFailureUrl("/login?error=true");
@@ -33,6 +39,10 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
         final Locale locale = localeResolver.resolveLocale(request);
 
         String errorMessage = messages.getMessage("message.badCredentials", null, locale);
+
+        if (loginAttemptService.isBlocked()) {
+            errorMessage = messages.getMessage("auth.message.blocked", null, locale);
+        }
 
         if (exception.getMessage()
             .equalsIgnoreCase("User is disabled")) {
